872abccda884570abb0154d76a8714514f99338e
Stephen Roy <sroy9424@yahoo.com>
2023-02-20 12:13:01 -0500

    Implement Tag API

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,10 +2,16 @@
 Tests for models.
 """
 from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 from core import models
+
+
+def create_user(email='user@example.com', password='testpass123'):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -65,3 +71,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
